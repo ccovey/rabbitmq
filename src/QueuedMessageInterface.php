d873b8309e6eda91a68fb6d764c6d3f4a7435859
ccovey <ccovey@opensky.com>
2016-12-09 17:01:29 -0600

    Add change in method signature to the interface also

--- src/QueuedMessageInterface.php
+++ src/QueuedMessageInterface.php
@@ -2,12 +2,14 @@
 
 namespace Ccovey\RabbitMQ;
 
+use Throwable;
+
 interface QueuedMessageInterface
 {
     public function getQueueName() : string;
     public function getDeliveryTag() : string;
     public function getBody() : array;
     public function getRawBody() : string;
-    public function fail();
+    public function fail(Throwable $throwable = null);
     public function isFailed() : bool;
 }
